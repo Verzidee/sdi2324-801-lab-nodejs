fad86435ecb42925ff9b9d8c37e827cd08e1c2ef
Zohaib Akhtar Kausar <zohaib0614@gmail.com>
2024-03-17 17:58:13 +0100

    SDI-2324-801-9.4-Registro de usuario, autenticación y uso de sesión.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -1,59 +1,66 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
-let bodyParser = require('body-parser');
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
-let app = express();
-let fileUpload = require('express-fileupload');
-app.use(fileUpload({
-  limits: { fileSize: 50 * 1024 * 1024 },
-  createParentPath: true
-}));
-app.set('uploadPath', __dirname)
-
+// Module Imports
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const createError = require('http-errors');
+const fileUpload = require('express-fileupload');
+const crypto = require('crypto');
 const { MongoClient } = require("mongodb");
-const connectionStrings = "mongodb+srv://admub:9z6W6MpkeRBx5uIH@musicstoreapp.njk8cnz.mongodb.net/?retryWrites=true&w=majority&appName=musicstoreapp";
-const dbClient = new MongoClient(connectionStrings);
-let songsRepository = require("./repositories/songsRepository");
-songsRepository.init(app,dbClient);
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const expressSession = require('express-session');
+const app = express();
 
 
-require("./routes/authors")(app);
-require("./routes/songs")(app, songsRepository);
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
+app.set('uploadPath', __dirname);
+app.set('clave', 'abcdefg');
+app.set('crypto', crypto);
 
+// Middleware Configuration
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload({
+  limits: { fileSize: 50 * 1024 * 1024 },
+  createParentPath: true
+}));
+app.use(expressSession({
+  secret: 'abcdefg',
+  resave: true,
+  saveUninitialized: true
+}));
 
+// Database Configuration
+const connectionString = "mongodb+srv://admub:9z6W6MpkeRBx5uIH@musicstoreapp.njk8cnz.mongodb.net/?retryWrites=true&w=majority&appName=musicstoreapp";
+const dbClient = new MongoClient(connectionString);
+const songsRepository = require("./repositories/songsRepository");
+const usersRepository = require("./repositories/usersRepository");
+songsRepository.init(app, dbClient);
+usersRepository.init(app, dbClient);
+
+// Routes Setup
+const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+require("./routes/users")(app, usersRepository);
+require("./routes/authors")(app);
+require("./routes/songs")(app, songsRepository);
 
-// catch 404 and forward to error handler
+// Error Handling
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
+// Export the App
 module.exports = app;
